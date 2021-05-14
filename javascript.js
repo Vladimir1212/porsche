7cb3f7538116aec6fd05a4f146518f362c278b4f
Vladimir <woyaxx@gmail.com>
2021-05-14 15:21:52 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('.slider').slick({
+        prevArrow: $(".pp2"),
+        nextArrow: $(".nn2"),
+        centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 2,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    }
+  ]
+    });
+})
